d80f95db3b87f1d3d53c4c4a225b60eed90ffdc4
Anna Morozova <anna.mik.morozova@gmail.com>
2020-11-13 01:18:15 -0500

    Feat: handle social media icons with links

--- src/Footer.js
+++ src/Footer.js
@@ -5,24 +5,31 @@ import {ImFacebook2} from "react-icons/im";
 import {FaTwitterSquare} from "react-icons/fa"
 import {ImInstagram} from "react-icons/im";
 import {ImLinkedin} from "react-icons/im";
+import {FaPinterestSquare} from "react-icons/fa";
+import {ImYoutube} from "react-icons/im";
 
 class Footer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             description: this.props.description,
-            socialMedia: this.props.socialMedia,
             resourcesName: this.props.resourcesName,
             resources: this.props.resources,
-            copyright: this.props.copyright
+            copyright: this.props.copyright,
+            linkedin: this.props.linkedin,
+            facebook: this.props.facebook,
+            twitter: this.props.twitter,
+            instagram: this.props.instagram,
+            youtube: this.props.youtube,
+            pinterest: this.props.pinterest
         }
     }
+
 	render() {
 		return (
             <div className="footer-container">
 
                 <div className="first-row">
-
                     <div className="col-about">
                         <div>
                             <div className="titles">About</div>
@@ -43,10 +50,12 @@ class Footer extends React.Component {
                 <div className="social-media-col col">
                     <div className="titles">Stay connected:</div>
                     <div className="social-media">
-                        <a href="#" target="_blank" className="socialMediaLogo"><ImFacebook2 color="black" size={25}/> </a>
-                        <a href="#" target="_blank" className="socialMediaLogo"><FaTwitterSquare color="black"size={25}/> </a>
-                        <a href="#" target="_blank" className="socialMediaLogo"><ImInstagram color="black" size={25}/> </a>
-                        <a href="#" target="_blank" className="socialMediaLogo"><ImLinkedin color="black" size={25}/> </a>
+                        {this.state.facebook != undefined ? <a href={`https://www.facebook.com/${this.state.facebook}`} target="_blank" className="socialMediaLogo"><ImFacebook2 color="black" size={25}/> </a> : ""}
+                        {this.state.linkedin != undefined ? <a href={`https://www.linkedin.com/in/${this.state.linkedin}`} target="_blank" className="socialMediaLogo"><FaTwitterSquare color="black"size={25}/> </a> : ""}
+                        {this.state.instagram != undefined ? <a href={`https://www.instagram.com/${this.state.instagram}`} target="_blank" className="socialMediaLogo"><ImInstagram color="black" size={25}/> </a> : ""}
+                        {this.state.twitter != undefined ? <a href={`https://www.twitter.com/${this.state.twitter}`} target="_blank" className="socialMediaLogo"><ImLinkedin color="black" size={25}/> </a> : ""}
+                        {this.state.pinterest != undefined ? <a href={`https://www.pinterest.com/${this.state.pinterest}`} target="_blank" className="socialMediaLogo"><FaPinterestSquare color="black" size={25}/> </a> : ""}
+                        {this.state.youtube != undefined ? <a href={`https://www.youtube.com/channel/${this.state.youtube}`} target="_blank" className="socialMediaLogo"><ImYoutube color="black" size={25}/> </a> : ""}
                     </div>
                 </div>
 
@@ -60,12 +69,12 @@ class Footer extends React.Component {
 
 Footer.propTypes = {
     description: PropTypes.string,
-    socialMedia: PropTypes.arrayOf(
-        PropTypes.shape({
-            socialMedianame: PropTypes.string,
-            socialMediaUrl: PropTypes.string
-        })
-    ),
+    linkedin: PropTypes.string,
+    instagram: PropTypes.string,
+    facebook: PropTypes.string,
+    linkedin: PropTypes.string,
+    youtube: PropTypes.string,
+    pinterest: PropTypes.string,
     resourcesName: PropTypes.string,
     resources: PropTypes.arrayOf(PropTypes.string),
     copyright: PropTypes.string
