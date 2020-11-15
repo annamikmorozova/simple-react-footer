c8a99f1b03be414946894f7ba800e955deb2ad3f
Anna Morozova <anna.mik.morozova@gmail.com>
2020-11-15 17:40:46 -0500

    Feat: add another menu col

--- src/Footer.js
+++ src/Footer.js
@@ -19,24 +19,35 @@ class Footer extends React.Component {
 
                 <div className="first-row">
                     <div className="col-about">
-                        <div>
-                                <div style={{ color: this.props.fontColor || "black" }} className="titles">{this.props.title1}</div> 
+                            <div style={{ color: this.props.fontColor || "black" }} className="first-title">{this.props.title1}</div> 
                             <div style={{ color: this.props.fontColor || "black" }} className="description">{this.props.description}</div>
-                        </div>
                     </div>
 
-                    <div>
-                        <div style={{ color: this.props.fontColor || "black" }} className="titles">{this.props.title2}</div>
-                        {this.props.resources.map(resource => (
+                    {this.props.col2 != undefined ?
+                    <div style={{marginRight: "45px"}}>
+                        <div style={{ color: this.props.fontColor || "black" }} className="second-title">{this.props.title2}</div>
+                        {this.props.col2.map(resource => (
                             <div key={resource.id}>
                                 <a href={`${resource.link}`} target="_blank" style={{ color: this.props.fontColor || "black" }} className="resources">{resource.name}</a>
                             </div>
                         ))}
                     </div>
-                </div>
+                    : "" }
+
+                    {this.props.col3 != undefined ?
+                    <div>
+                        <div style={{ color: this.props.fontColor || "black" }} className="third-title">{this.props.title3}</div>
+                        {this.props.col3.map(resource => (
+                            <div key={resource.id}>
+                                <a href={`${resource.link}`} target="_blank" style={{ color: this.props.fontColor || "black" }} className="resources">{resource.name}</a>
+                            </div>
+                        ))}
+                    </div>
+                    : "" }
+                </div> 
 
                 <div className="social-media-col col">
-                    <div style={{ color: this.props.fontColor || "black" }} className="titles">Stay connected</div>
+                    <div style={{ color: this.props.fontColor || "black" }} className="stay-connected-title">Stay connected</div>
                     <div className="social-media">
                         {this.props.facebook != undefined ? <a href={`https://www.facebook.com/${this.props.facebook}`} target="_blank" className="socialMediaLogo"><ImFacebook2 color={`${this.props.iconColor || "black" }`} size={25}/> </a> : ""}
                         {this.props.linkedin != undefined ? <a href={`https://www.linkedin.com/in/${this.props.linkedin}`} target="_blank" className="socialMediaLogo"><FaTwitterSquare color={`${this.props.iconColor || "black" }`} size={25}/> </a> : ""}
@@ -65,7 +76,14 @@ Footer.propTypes = {
     pinterest: PropTypes.string,
     title1: PropTypes.string,
     title2: PropTypes.string,
-    resources: PropTypes.arrayOf(
+    title3: PropTypes.string,
+    col2: PropTypes.arrayOf(
+        PropTypes.shape({
+            name: PropTypes.string,
+            link: PropTypes.string
+        })
+    ),
+    col3: PropTypes.arrayOf(
         PropTypes.shape({
             name: PropTypes.string,
             link: PropTypes.string
